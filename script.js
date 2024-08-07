50b8f8572be4c5c44b699a70b7d5a3eba12dc183
qkenn <github.com.sampling990@passinbox.com>
2024-08-07 15:00:22 +0530

    feat: create a blueprint for form field properties and checking it's validity

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
-const form = document.getElementById('form');
-const emailInput = document.getElementById('email');
-const errorMsg = document.getElementById('error-msg');
+// class Form {
+//   constructor() {
+//     this.form = document.getElementById('form');
+//     this.submitBtn = document.getElementById('submit');
 
-emailInput.addEventListener('input', () => {
-  if (emailInput.validity.valueMissing) {
-    errorMsg.textContent = 'email required';
-  } else if (emailInput.validity.typeMismatch) {
-    errorMsg.textContent = 'enter a valid email';
-  } else {
-    errorMsg.textContent = '';
+//     this.username = document.getElementById('username');
+//     this.email = document.getElementById('email');
+//     this.zip = document.getElementById('zip');
+//     this.password = document.getElementById('password');
+//     this.confirm = document.getElementById('confirm');
+//   }
+
+//   handleInput = () => {
+//     this.username.addEventListener('input', () => {
+//       this.validateUsername();
+//     });
+//   };
+
+//   validateUsername = () => {
+//     const validityState = this.username.validity;
+
+//     switch (true) {
+//       case validityState.valueMissing:
+//         console.log('value missing');
+//         break;
+//     }
+//   };
+// }
+
+class FormField {
+  constructor(element) {
+    this.element = element;
+    this.validityState = element.validity;
+    this.valid = false;
+    this.init();
   }
 
-  if (!emailInput.validity.valid) {
-    emailInput.style.outline = '2px solid red';
-  } else {
-    emailInput.style.outline = '2px solid green';
-  }
-});
+  init = () => {
+    this.element.addEventListener('input', () => {
+      this.validate();
+    });
+  };
 
-form.addEventListener('click', (e) => {
-  if (!emailInput.validity.valid) {
-    e.preventDefault();
-  }
-});
+  validate = () => {
+    if (this.element.validity.valid) {
+      this.valid = true;
+      console.log('input is valid');
+
+      return;
+    }
+
+    switch (true) {
+      case this.validityState.valueMissing:
+        console.log('value missing');
+        break;
+      case this.validityState.typeMismatch:
+        console.log('wrong type');
+        break;
+      case this.validityState.patternMismatch:
+        console.log('not match the pattern');
+        break;
+      case this.validityState.tooShort:
+        console.log('too short');
+        break;
+      case this.validityState.tooLong:
+        console.log('too long');
+        break;
+    }
+  };
+}
+
+const username = new FormField(document.getElementById('username'));
+const email = new FormField(document.getElementById('email'));
+const country = new FormField(document.getElementById('country'));
+const zip = new FormField(document.getElementById('zip'));
+const password = new FormField(document.getElementById('password'));
+const confirm = new FormField(document.getElementById('confirm'));
